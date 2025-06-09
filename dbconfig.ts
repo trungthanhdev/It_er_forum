407ae90120e5e0ca7f92216c7a137f3ddd1f0ed6
trungthanh324 <136559582+trungthanh324@users.noreply.github.com>
2025-06-09 18:34:08 +0700

    comment/import

--- dbconfig.ts
+++ dbconfig.ts
@@ -1,34 +1,12 @@
-// import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
-
-// export const pgConfig : PostgresConnectionOptions ={
-//     type: "postgres",
-//     url: process.env.DATABASE_URL || "",
-//     port: process.env.DATABASE_PORT ? parseInt(process.env.DATABASE_PORT, 10) : 5432,
-//     entities:[],
-//     synchronize: true
-// }
-
-
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as dotenv from 'dotenv';
 
-dotenv.config(); 
+dotenv.config();
 
 export const pgConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: Number(process.env.POSTGRES_PORT) || 5432, 
-  username: process.env.POSTGRES_USER || 'postgres',
-  password: process.env.POSTGRES_PASSWORD || '',
-  database: process.env.POSTGRES_DB || 'postgres',
-  ssl: { rejectUnauthorized: false }, // Bắt buộc khi kết nối đến Neon.tech
+  url: process.env.DATABASE_URL, 
+  ssl: { rejectUnauthorized: false },
   autoLoadEntities: true,
   synchronize: true,
-  extra: {
-    client_encoding: "UTF8",
-  },
-//   entities: [__dirname+'/**/*.entitiy{.ts,.js}']
-
 };
-
-
