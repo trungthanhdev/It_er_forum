0179f2de761d56cb1569d8abe4bcfd729b04ba93
trungthanh324 <136559582+trungthanh324@users.noreply.github.com>
2025-07-02 15:25:35 +0700

    Feature/Keep-Server-Alive

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+   @Get('keep-awake')
+  keepAwake() {
+    console.log('Keep awake ping received at', new Date().toISOString());
+    return { message: 'I am awake!' };
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
+  httpService: any;
   getHello(): string {
     return 'Hello World!';
+  }
+  @Cron('*/14 * * * *') 
+  async pingSelf() {
+    const url = 'https://it-er-forum.onrender.com/keep-awake'; 
+    try {
+      await firstValueFrom(this.httpService.get(url));
+      console.log(' Ping self to stay awake');
+    } catch (error) {
+      console.error(' Error ping self:', error.message);
+    }
   }
 }
